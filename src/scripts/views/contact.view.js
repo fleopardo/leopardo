7a8c4d2df6b72a71ea0acdcefa69f40a04f34ac2
sleopardo <santiagoleopardo@gmail.com>
2016-06-11 18:59:29 -0300

    products page, without links

--- src/scripts/views/contact.view.js
+++ src/scripts/views/contact.view.js
@@ -8,11 +8,6 @@ App.module('Hosepower.Views', function (Views, App, Backbone, Marionette, $, _) 
             //GOOGLE MAPS
         	if( typeof(google) != "undefined"){
 
-// -34.5760088
-// -58.4977576
-//
-// -34.5760132
-// -58.4955689
         		var options = {
         			zoom: 15,
         			center: new google.maps.LatLng(-34.5760088,-58.4977576),
