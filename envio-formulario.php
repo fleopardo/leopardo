9e645532bc50d04c7707913d4f3eb507dbea3e4d
sleopardo <santiagoleopardo@gmail.com>
2016-06-28 20:13:39 -0300

    elimine dependencia vieja

--- envio-formulario.php
+++ envio-formulario.php
@@ -48,35 +48,15 @@
 	//Header
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
-	$headers .= "From: ".$email."\r\n";
-
-	//header Copy
-	$headersCopy = "MIME-Version: 1.0\r\n";
-	$headersCopy .= "Content-type: text/plain; charset=UTF-8\r\n";
-	$headersCopy .= "From: noreply@hosepower.com.ar \r\n";
-
+	$headers .= 'From:' . $email . "\r\n";
+	$headers .= "Reply-To: " . $email . "\r\n";
+	$headers .= "Return-path: " . $email;
 
 	//verifico que el mail se haya enviado con exito
-	if(mail("$para", "$asunto", "$datos", "$headers")){
+	if( mail("$para", "$asunto", "$datos", "$headers") ){
 		$mensaje['success']=true;
 	}else{
 		$mensaje['success']=false;
-	}
-
-	//devuelvo mensaje, para saber si fue true o false
-	if(!function_exists('json_encode'))
-	{
-		include_once('includes/JSON.php');
-		$GLOBALS['JSON_OBJECT'] = new Services_JSON();
-		function json_encode($value)
-		{
-			return $GLOBALS['JSON_OBJECT']->encode($value);
-		}
-
-		function json_decode($value)
-		{
-			return $GLOBALS['JSON_OBJECT']->decode($value);
-		}
 	}
 
 	echo json_encode($mensaje);
