3d837a6a97ec910dc3eb2b2312e2fd0c14de520a
fleopardo <fleopardo@gmail.com>
2016-07-27 21:16:17 -0300

    productos desde la home

--- src/scripts/views/products.view.js
+++ src/scripts/views/products.view.js
@@ -6,6 +6,24 @@ App.module('Hosepower.Views', function (Views, App, Backbone, Marionette, $, _) 
 
         onShow: function() {
 
+        	var categoryReceive = window.document.location.hash.split('?')[1];
+
+        	if (categoryReceive) {
+
+        		setTimeout(function() {
+	        		var $container = $('[data-js="' + categoryReceive + '"]');
+	        		$container[0].scrollIntoView();
+	        	
+	        		$container.addClass('blink');
+	        		setTimeout(function() {
+	        			$container.removeClass('blink');
+	        		}, 1000)
+
+	        	}, 100);
+        		
+
+        	}
+
         }
     });
 
